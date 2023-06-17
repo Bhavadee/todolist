622cc97c6b46d420086462359569b6d7b5678e74
bhavadeep reddy <bhavadeep2003@gmail.com>
2023-06-17 14:46:04 -0700

    commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+var ejs = require('ejs');
+const port = 9930;
+
+const app = express();
+var items = ["buy","make","Eat"];
+app.set("view engine", 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  var day = new Date();
+  
+  var options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  };
+  var da = day.toLocaleDateString("en-IN",options);
+    res.render("list", {title:da , newTasks:items});
+  
+    
+});
+ app.post("/", (req,res) =>{
+   items.push(req.body.listItem);
+   res.redirect("/")
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
